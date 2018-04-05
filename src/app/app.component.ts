8083b4d0090ae30c978661f618fcd773dc059b1c
Pavel Gurov <lucius.gu@ya.ru>
2018-04-05 23:56:23 +0700

    CSS improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,9 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     template: `
-
         <router-outlet></router-outlet>
-
-  `,
+    `,
     styles: []
 })
 export class AppComponent {
-    title = 'app';
 }
