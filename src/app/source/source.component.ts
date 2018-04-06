1b2df49a719c3ab9c7b641fc9daddea3326c6bce
Pavel Gurov <lucius.gu@ya.ru>
2018-04-06 10:42:15 +0700

    Added loader

--- src/app/source/source.component.ts
+++ src/app/source/source.component.ts
@@ -5,27 +5,28 @@ import { Feed, Post } from '../models';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/finally';
 
 @Component({
     selector: 'app-source',
-    templateUrl: 'source.component.html',
-    styles: [`
-  `]
+    templateUrl: 'source.component.html'
 })
 export class SourceComponent implements OnInit {
 
     feed: Feed = new Feed();
     newPosts: Post[] = [];
     showNewPosts: boolean = false;
+    loading: boolean = false;
 
     constructor(private newsService: NewsService, private route: ActivatedRoute) {
     }
 
     ngOnInit() {
+        this.loading = true;
         this.route.params
             .map(params => decodeURIComponent(params['url']))
             .do(url => this.feed = this.newsService.getFeed(url))
-            .switchMap(url => this.newsService.getNewPostsAndUpdate(url))
+            .switchMap(url => this.newsService.getNewPostsAndUpdate(url).finally(() => this.loading = false))
             .subscribe(posts => this.newPosts = posts);
     }
 }
