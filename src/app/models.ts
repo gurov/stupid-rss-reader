d72292f5414fc6bcfe81b8fbb56c7741ff5bb7bb
gurov <lucius.gu@ya.ru>
2018-10-05 18:28:57 +0700

    small fixes

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,17 +1,10 @@
 export class Post {
-    creator: string;
     title: string;
     link: string;
     author: string;
     description: string;
-    contentSnippet: string;
     categories: string[];
     pubDate: string;
-    datejs: Date;
-    enclosure?: {
-        length: number,
-        type: string,
-        url: string
-    }
+    date: Date;
 }
 
