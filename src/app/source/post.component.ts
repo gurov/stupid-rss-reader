c8d2f81ff7c77c87040d0d4bc08ed62dac884638
gurov <lucius.gu@ya.ru>
2018-12-11 21:26:00 +0700

    feat: old post is hidden by default

--- src/app/source/post.component.ts
+++ src/app/source/post.component.ts
@@ -10,13 +10,16 @@ import { Post } from '../models';
             </span>
       {{post.isoDate ? (post.isoDate|isoToDate|date:'yyyy-MM-dd HH:mm') : ''}}
     </h6>
-    <h4>
+    <h4 *ngIf="hidden" (click)="hidden = !hidden" class="text-muted">
+      {{post.title}}<sup>⬍</sup>
+    </h4>
+    <h4 *ngIf="!hidden">
       <a target="_blank" [href]="post.link">{{post.title}}</a><sup> ↗</sup>
     </h4>
-    <div *ngIf="post?.categories?.length" class="small text-muted mb-1">
+    <div *ngIf="post?.categories?.length && !hidden" class="small text-muted mb-1">
       <em *ngFor="let cat of post.categories; let last = last">{{cat}}<span *ngIf="!last">, </span></em>
     </div>
-    <div class="post-container">
+    <div class="post-container" *ngIf="!hidden">
       <div [innerHTML]="post.description | sanitizeHtml"></div>
     </div>
   `,
@@ -25,6 +28,8 @@ import { Post } from '../models';
 export class PostComponent {
 
   @Input() post: Post = new Post();
+  @Input() fresh: boolean = false;
+  hidden: boolean = !this.fresh; 
 
   constructor() {
   }
